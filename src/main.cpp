71d3006ba2c2debdbf8484158f69ee5bce97720e
Ramon Melo <ramonmelo@ramonmelo.com>
2025-07-06 18:02:27 +0200

    Added: Basic Project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <AudioTools.h>
+
+// #define NUM_LEDS 1
+// #define DATA_PIN GPIO_NUM_21
+// CRGB leds[NUM_LEDS];
+
+void setup()
+{
+  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+}
+
+void loop()
+{
+}
